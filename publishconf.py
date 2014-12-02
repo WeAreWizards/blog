d2650005c113ee136cb0c22b89890823b2ad519e
Tom Hunger <tehunger@gmail.com>
2014-12-02 10:25:51 +0000

    Disable CATEGORY_FEED_ATOM for now.
    It breaks when we have no categories because pelican
    tries to write the category  twice.

--- publishconf.py
+++ publishconf.py
@@ -14,7 +14,7 @@ SITEURL = 'https://blog.wearewizards.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+# CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
